b207b50ae657c6630eddb14aa4ae9056cb9bef8c
Alexandr <a.platcev@ya.ru>
2023-03-11 10:04:18 +0300

    add solution for 3-module 3-task

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -10,8 +11,29 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+
   switch (req.method) {
     case 'GET':
+
+      const stream = fs.createReadStream(filepath);
+      stream.pipe(res);
+
+      stream.on('error', (error) => {
+        const reg = /\w+\//gi;
+
+        if (error.code === 'ENOENT') {
+          if (reg.test(pathname)) {
+            res.statusCode = 400;
+            res.end('Вложенные пути не поддерживаются');
+          } else {
+            res.statusCode = 404;
+            res.end(`Файл не найден ${filepath}`);
+          }
+        } else {
+          res.statusCode = 500;
+          res.end();
+        }
+      });
 
       break;
 
