6943aa87d35a3e9f74ef4bd143cd7461b50e2e76
Alexandr <a.platcev@ya.ru>
2023-03-11 18:29:44 +0300

    add solution for 3-module 4-task

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,6 +1,8 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const LimitSizeStream = require('./LimitSizeStream');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -12,9 +14,53 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
+      if (fs.existsSync(filepath)) {
+        res.statusCode = 409;
+        res.end('Such file already exists');
+        break;
+      }
+
+      const limitedStream = new LimitSizeStream({limit: 1048576});
+      const outStream = fs.createWriteStream(filepath);
+      limitedStream.pipe(outStream);
+
+      req.on('data', (chunk) => {
+        limitedStream.write(chunk);
+      });
+
+      req.on('end', () => {
+        limitedStream.end();
+        res.statusCode = 201;
+        res.end('Все ок');
+      });
+
+      req.on('aborted', () => {
+        limitedStream.destroy();
+        fs.unlinkSync(filepath);
+      });
+
+      outStream.on('error', (error) => {
+        const reg = /\w+\//gi;
+        if (error.code === 'ENOENT') {
+          if (reg.test(pathname)) {
+            res.statusCode = 400;
+            res.end('Вложенные пути не поддерживаются');
+          }
+        }
+      });
+
+      limitedStream.on('error', (error) => {
+        if (error.code === 'LIMIT_EXCEEDED') {
+          fs.unlinkSync(filepath);
+          res.statusCode = 413;
+          res.end('Тяжело');
+        } else {
+          res.statusCode = 500;
+          res.end();
+        }
+      });
 
       break;
-
     default:
       res.statusCode = 501;
       res.end('Not implemented');
