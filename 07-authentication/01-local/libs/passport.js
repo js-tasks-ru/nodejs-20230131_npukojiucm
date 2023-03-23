10e92864503499e6d0d3c59a578aa65ab878e0b3
Aleksandr Platcev <a.platcev@yandex.ru>
2023-03-23 22:22:23 +0300

    add solution for 6-module 2-task

--- 07-authentication/01-local/libs/passport.js
+++
@@ -1,8 +0,0 @@
-const {KoaPassport} = require('koa-passport');
-const passport = new KoaPassport();
-
-const localStrategy = require('./strategies/local');
-
-passport.use(localStrategy);
-
-module.exports = passport;
--- 07-authentication/01-local/libs/strategies/local.js
+++
@@ -1,8 +0,0 @@
-const LocalStrategy = require('passport-local').Strategy;
-
-module.exports = new LocalStrategy(
-    {usernameField: 'email', session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
-    },
-);
