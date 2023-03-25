b1777e3272f9712e0dcd21bafb6b48061bfc6bba
Aleksandr Platcev <a.platcev@ya.ru>
2023-03-25 21:06:29 +0300

    ...

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,10 +1,12 @@
 const User = require('../../models/User');
 
 module.exports = function authenticate(strategy, email, displayName, done) {
+  if (email) {
   User.findOne({email: email}, async function (err, user) {
     if (!user) return done(null, false, 'Пользователь не найден');
 
     return done(null, user, 'Все ок');
   })
-  
+  }
+  return done(null, false, 'Забыли передать почту?')
 };
