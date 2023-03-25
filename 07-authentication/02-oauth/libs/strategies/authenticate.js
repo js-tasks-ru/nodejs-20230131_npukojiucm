67f07a520a051804e1d46835471fb70aa8245c6e
Aleksandr Platcev <a.platcev@ya.ru>
2023-03-25 21:10:18 +0300

    повторный тест

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,12 +1,12 @@
 const User = require('../../models/User');
 
 module.exports = function authenticate(strategy, email, displayName, done) {
-  if (email) {
+  if (email) return done(null, false, 'Забыли передать почту?');
+  
   User.findOne({email: email}, async function (err, user) {
     if (!user) return done(null, false, 'Пользователь не найден');
 
     return done(null, user, 'Все ок');
   })
-  }
-  return done(null, false, 'Забыли передать почту?')
+  
 };
