e16528797d3aa63845b47472d9d2ee3e32ce74d6
Aleksandr Platcev <a.platcev@ya.ru>
2023-03-25 23:10:47 +0300

    add solution 7-module 2-task

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,15 +1,31 @@
 const User = require('../../models/User');
 
-module.exports = function authenticate(strategy, email, displayName, done) {
-  if (!email) return done(null, false, 'Не указан email');
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  try {
+    if (!email) return done(null, false, 'Не указан email');
 
-  User.findOne({email: email}, async function (err, user) {
+    const user = await User.findOne({email: email});
+
     if (!user) {
-      const newUser = new User({email: email, displayName: displayName});
-      if (err) return done(err, false);
+      const newUser = new User({
+        email: email,
+        displayName: displayName,
+      });
+      await newUser.save();
+      return done(null, newUser);
     }
 
-    return done(null, user, 'Все ок');
-  })
+    return done(null, user);
+  } catch (err) {
+    done(err);
+  }
+  // User.findOne({email: email}, async function (err, user) {
+  //   if (!user) {
+  //     const newUser = new User({email: email, displayName: displayName});
+  //     if (err) return done(err, false);
+  //   }
+
+  //   return done(null, user, 'Все ок');
+  // })
   
 };
