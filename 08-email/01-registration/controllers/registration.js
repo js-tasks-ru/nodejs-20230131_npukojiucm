ca9833e55adfdf2adbd07956442d80cf95608c09
Aleksandr Platcev <a.platcev@ya.ru>
2023-04-22 00:48:59 +0300

    add solution 8 module 1 task

--- 08-email/01-registration/controllers/registration.js
+++ 08-email/01-registration/controllers/registration.js
@@ -1,18 +1,70 @@
-const { v4: uuid } = require('uuid');
+const {v4: uuid} = require('uuid');
 const User = require('../models/User');
+const Session = require('../models/Session');
 const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
+  // await User.deleteMany();
+  // await Session.deleteMany();
   const token = uuid();
 
   const {email, displayName, password} = ctx.request.body;
 
-  const newUser = await User.create({
-    
-  })
+  if (await User.findOne({email}) !== null) {
+    throw {
+      status: 400,
+      message: {
+        email: 'Такой email уже существует',
+      },
+    };
+  }
 
+  const newUser = await new User({
+    email,
+    displayName,
+    verificationToken: token,
+  });
+  await newUser.setPassword(password);
+  await newUser.save();
+
+  await sendMail({
+    template: 'confirmation',
+    locals: {token: `${token}`},
+    to: newUser.email,
+    subject: 'Подтвердите почту',
+  });
+
+  ctx.body = {status: 'ok'};
 };
 
 module.exports.confirm = async (ctx, next) => {
+  const {verificationToken} = ctx.request.body;
 
+  const user = await User.findOneAndUpdate({
+    verificationToken,
+  },
+  {
+    $unset:
+        {
+          verificationToken: '',
+        },
+  });
+
+  if (user === null) {
+    throw {
+      status: 400,
+      message: 'Ссылка подтверждения недействительна или устарела',
+    };
+  }
+
+  const sessionToken = uuid();
+  await Session.create({
+    token: sessionToken,
+    lastVisit: new Date(),
+    user: user.id,
+  });
+
+  ctx.body = {
+    token: '',
+  };
 };
