624356d53240740e73ca0585dee45eed9bf70df3
Aleksandr Platcev <a.platcev@ya.ru>
2023-04-21 19:12:29 +0300

    add 8-1

--- 08-email/01-registration/controllers/registration.js
+++ 08-email/01-registration/controllers/registration.js
@@ -3,6 +3,13 @@ const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
+  const token = uuid();
+
+  const {email, displayName, password} = ctx.request.body;
+
+  const newUser = await User.create({
+    
+  })
 
 };
 
